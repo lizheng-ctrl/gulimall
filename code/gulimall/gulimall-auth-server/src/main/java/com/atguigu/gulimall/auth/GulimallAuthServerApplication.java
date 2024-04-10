929c1741c4abb88afabee07cda6058c8ea552a93
ken <3087860936@qq.com>
2024-04-10 11:18:22 +0800

    springSession共享

--- code/gulimall/gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
+++ code/gulimall/gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 
 @EnableDiscoveryClient
+@EnableRedisHttpSession
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.auth.feign")
 @SpringBootApplication
 public class GulimallAuthServerApplication {
